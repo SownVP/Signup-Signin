fcba56cd82afcdd4678a3f16b356f8f52c4bf8fa
TCODEDAO <thanhguitar282@gmail.com>
2022-04-23 14:54:31 +0700

    sua file js

---
+++ main.js
@@ -0,0 +1,12 @@
+
+    const signUpButton = document.querySelector('#signUp');
+    const signInButton = document.querySelector('#signIn');
+    const container = document.querySelector('.container');
+console.log(signUpButton,signInButton,container)
+    signUpButton.addEventListener('click', () => {
+        container.classList.add('right-panel-active');
+    });
+
+    signInButton.addEventListener('click', () => {
+        container.classList.remove('right-panel-active');
+    });
